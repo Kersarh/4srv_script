00b7b0cd8b96aa276ddbe4075560a7a34dacdcfa
kers3412 <kers3412.git@gmail.com>
2021-02-20 17:03:35 +0300

    f_run

---
+++ first_run_ubuntu_desktop.sh
@@ -0,0 +1,56 @@
+﻿#!/bin/bash
+
+echo "Скрипт обновит систему и установит некторые дополнения."
+echo -n "Продолжить? (y/n) "
+read item
+case "$item" in
+    y|Y) echo "Ввели «y», продолжаем..."
+        ;;
+    n|N) echo "Ввели «n», завершаем..."
+        exit 0
+        ;;
+    *) echo "Ничего не ввели."
+		exit 0
+        ;;
+esac
+
+# Обновление ОС
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install -y python3
+apt-get install -y net-tools
+
+# Дополнительные архиваторы
+apt-get install -y p7zip-rar p7zip-full rar unrar arj cabextract file-roller zip unzip
+apt-get install -y nano mosh tmux htop git curl wget gcc build-essential make
+
+# Мультимедия кодеки
+apt-get install -y ubuntu-restricted-extras
+
+#
+sudo dpkg --configure -a
+
+#Небольшая чистка
+apt-get autoremove -y
+apt-get autoclean -y
+apt-get clean -y
+apt-get install -f -y
+
+#fish вместо Bash
+# apt install fish
+# set fish_greeting ""
+# chsh -s /usr/bin/fish
+# fish_config
+
+#Модули наутилус
+apt-get install -y nautilus-admin exe-thumbnailer
+cd ~/Templates/
+touch new-text.txt
+nautilus -q
+
+# Настройки гнома
+apt-get install -y gnome-tweak-tool
+
+# Beep о завершении
+echo $'\a'
---
+++ first_run_ubuntu_server.sh
@@ -0,0 +1,48 @@
+﻿#!/bin/bash
+
+# Обновление ОС
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install -y python3
+apt-get install -y net-tools
+
+# Дополнительные архиваторы
+apt-get install -y p7zip-rar p7zip-full rar unrar unace arj cabextract file-roller zip unzip
+# Для разработки
+apt-get install -y tree redis-server libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-pip python3-pip python3-dev python-imaging python3-lxml libxslt-dev python-libxml2 python-libxslt1 libffi-dev libssl-dev python-dev gnumeric libsqlite3-dev libpq-dev libxml2-dev libxslt1-dev libjpeg-dev libfreetype6-dev libcurl4-openssl-dev
+#
+apt-get install -y nano mosh tmux htop git curl wget gcc build-essential make
+
+#
+apt-get install -y supervisor nginx
+
+# Локали
+export LANGUAGE=ru_RU.UTF-8
+export LANG=ru_RU.UTF-8
+export LC_ALL=ru_RU.UTF-8
+sudo locale-gen ru_RU.UTF-8
+
+#
+sudo dpkg --configure -a
+
+# Небольшая чистка
+apt-get autoremove -y
+apt-get autoclean -y
+apt-get clean -y
+apt-get install -f -y
+
+# # fish вместо Bash
+# apt install fish
+# set fish_greeting ""
+# chsh -s /usr/bin/fish
+# # fish_config
+
+# ZSH вместо Bash
+apt install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /usr/bin/zsh
+
+
+# Beep о завершении
+echo $'\a'
